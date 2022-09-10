a78a51b173aa6c34f4954fe0c6d59cd9eabe1e5f
waleedrana777 <37755440+waleedrana777@users.noreply.github.com>
2022-09-11 01:36:37 +0200

    custom email api

--- src/firebase/db.ts
+++ src/firebase/db.ts
@@ -3,13 +3,13 @@ import { initializeFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_API_KEY,
-	authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_APP_ID,
-	measurementId: process.env.REACT_APP_MEASUREMENT_ID,
+	apiKey: process.env.REACT_APP_API_KEY?.trim(),
+	authDomain: process.env.REACT_APP_AUTH_DOMAIN?.trim(),
+	projectId: process.env.REACT_APP_PROJECT_ID?.trim(),
+	storageBucket: process.env.REACT_APP_STORAGE_BUCKET?.trim(),
+	messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID?.trim(),
+	appId: process.env.REACT_APP_APP_ID?.trim(),
+	measurementId: process.env.REACT_APP_MEASUREMENT_ID?.trim(),
 };
 
 const app = initializeApp(firebaseConfig);
